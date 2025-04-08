a1f397755592f5a9197bd240b98879db611e8071
Joseph Barnes <jobarn@microsoft.com>
2025-04-08 00:16:17 +0000

    Setup Django REST Framework

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-xeid(dcg#m&bbj$@ko+qkshmcbikw(dw+dwcp$-h$p^&yi%w42
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'miniature-space-cod-qprjgqrgx53445x-8000.app.github.dev']
 
 
 # Application definition
